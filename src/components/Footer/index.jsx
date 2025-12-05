ed66c294375928479acb173bbb6614d955a00b00
Waleed084 <raowaleed084@gmail.com>
2025-12-05 21:48:57 +0500

    fixed footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,42 +9,6 @@ export default function Footer() {
           <img src="logo neurai.svg" alt="NeurAI" className="footer-logo" />
           <div className="copyright">NeurAI © {new Date().getFullYear()}</div>
         </div>
-
-        <div className="footer-grid">
-          <div className="footer-col">
-            <h4>Get started</h4>
-            <ul>
-              <li>Sign up for free</li>
-              <li>Pricing</li>
-              <li>Request a demo</li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Solutions</h4>
-            <ul>
-              <li>Outbound</li>
-              <li>Inbound</li>
-              <li>Data Enrichment</li>
-              <li>Deal Execution</li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Resources</h4>
-            <ul>
-              <li>Apollo Academy</li>
-              <li>Insights</li>
-              <li>Partners</li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Company</h4>
-            <ul>
-              <li>About</li>
-              <li>Careers</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-        </div>
       </div>
     </footer>
   );
